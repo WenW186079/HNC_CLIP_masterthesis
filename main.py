6cdd880418285bfe96390383aebf8ae70448a394
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-06 19:07:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,24 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms  
 from torch.optim import AdamW
 import clip 
+import deepspeed  
 
 from load_data import HNCCLIPDataset, load_data_pairs 
 from Loss_func import HNC_Loss
-from train import train_clip_with_hnc_loss
+from train import train_clip_with_hnc_loss, push_vision_encoder_to_hub
 
 
+os.environ["TORCH_EXTENSIONS_DIR"] = "/mount/studenten/team-lab-cl/data2024/w/data/torch_extensions/"
 
 # Paths
 train_json_file_path = '/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_train_sampled_1_percent.json'
 val_json_file_path = '/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_val_sampled_1_percent.json'
 image_folder_path = '/mount/studenten/team-lab-cl/data2024/w/data/thes/gqa_dataset/images/images'
-batch_size = 3
+config_path = '/mount/studenten/team-lab-cl/data2024/w/data/thes/config/deepspeed_config.json'
 
-
+# Hyperparameters
+batch_size = 32
+epochs = 1
 
 train_loader = load_data_pairs(
     json_file_path=train_json_file_path, 
@@ -65,20 +69,28 @@ for batch_idx, (images, pos_captions, neg_captions, sources, image_paths) in enu
 '''
 
 # Load CLIP model: "ViT-B/16","ViT-B/32","ViT-L/14","ViT-L/14@336px"
-model, preprocess = clip.load("ViT-B/16", device=device)
+model, preprocess = clip.load("ViT-B/32", device=device)
+model.float()
 
-# 冻结文本编码器，只允许视觉编码器训练
+# Freeze text encoder; only train vision encoder
 for name, param in model.named_parameters():
     if "transformer" in name or "token_embedding" in name or "text_projection" in name:
         param.requires_grad = False
 
+# Save original parameters
 clip_params = {name: param.clone().detach() for name, param in model.named_parameters()}
-fisher_matrix = {name: torch.ones_like(param) * 0.01 for name, param in model.named_parameters()}
 
 # Define Loss and Optimizer
-criterion = HNC_Loss(fisher_matrix, clip_params, alpha=0.5, tau=0.07, lambda_=0.1)
-optimizer = AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=5e-6)
+criterion = HNC_Loss(clip_params, alpha=0.5, tau=0.07, lambda_=0.1)
 
-train_clip_with_hnc_loss(model, train_loader, val_loader, criterion, optimizer, device, epochs=1)
+# Train the model
+model_engine = train_clip_with_hnc_loss(
+    model,
+    train_loader,
+    val_loader,
+    criterion,
+    epochs=epochs,
+    config_path=config_path
+)
 
-push_vision_encoder_to_hub(model, "HNC_CLIP_ViT", "WenWW")
+push_vision_encoder_to_hub(model_engine.module, "HNC_CLIP_ViT", "WenWW")
