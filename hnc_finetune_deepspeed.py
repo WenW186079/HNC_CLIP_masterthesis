f6ae13e18af0f11c613243921e2b90b84437284b
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-25 00:25:17 +0100

    Update hnc_finetune_deepspeed.py

--- hnc_finetune_deepspeed.py
+++ hnc_finetune_deepspeed.py
@@ -1,12 +1,12 @@
 import logging
 import wandb
 import torch
-from PIL import Image  
-import math
-import time
-from transformers import CLIPModel, CLIPProcessor
-from huggingface_hub import HfApi, HfFolder, Repository
-from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from PIL import Image
+from transformers import CLIPModel, CLIPProcessor, get_cosine_schedule_with_warmup
+from huggingface_hub import HfApi
+import deepspeed.comm as dist
+import os
+
 '''
 
 This version added:
@@ -15,25 +15,60 @@ This version added:
 
 '''
 
+# def preprocess_text_and_images(batch, processor, device):
+#     """
+#     Preprocess the batch data: images, positive captions, and negative captions.
+#     """
+#     image_paths, pos_captions, neg_captions = batch
+#     images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+
+#     inputs = processor(
+#         images=images,
+#         text=pos_captions + neg_captions,
+#         return_tensors="pt",
+#         padding=True
+#     ).to(device)
+
+#     pos_text_inputs = inputs["input_ids"][:len(pos_captions)]
+#     neg_text_inputs = inputs["input_ids"][len(pos_captions):]
+
+#     return inputs["pixel_values"], pos_text_inputs, neg_text_inputs
+
 def preprocess_text_and_images(batch, processor, device):
     """
     Preprocess the batch data: images, positive captions, and negative captions.
+    Remove duplicate image paths within the batch.
     """
     image_paths, pos_captions, neg_captions = batch
-    images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+    # logging.info(f"Rank {dist.get_rank()} Original batch size: {len(image_paths)}")
+    
+    # Remove duplicate image paths and corresponding captions
+    seen = set()
+    unique_image_paths = []
+    unique_pos_captions = []
+    unique_neg_captions = []
 
+    for i, image_path in enumerate(image_paths):
+        if image_path not in seen:
+            seen.add(image_path)
+            unique_image_paths.append(image_path)
+            unique_pos_captions.append(pos_captions[i])
+            unique_neg_captions.append(neg_captions[i])
+
+    # logging.info(f"Rank {dist.get_rank()} Unique batch size: {len(unique_image_paths)}")
+
+    images = [Image.open(image_path).convert("RGB") for image_path in unique_image_paths]
     inputs = processor(
         images=images,
-        text=pos_captions + neg_captions,
+        text=unique_pos_captions + unique_neg_captions,
         return_tensors="pt",
         padding=True
     ).to(device)
 
-    pos_text_inputs = inputs["input_ids"][:len(pos_captions)]
-    neg_text_inputs = inputs["input_ids"][len(pos_captions):]
+    pos_text_inputs = inputs["input_ids"][:len(unique_pos_captions)]
+    neg_text_inputs = inputs["input_ids"][len(unique_pos_captions):]
 
     return inputs["pixel_values"], pos_text_inputs, neg_text_inputs
-
 
 def train_clip_model(model_engine, processor, data_loader, loss_fn, optimizer, num_epochs, device,learning_rate):
     """
@@ -41,25 +76,22 @@ def train_clip_model(model_engine, processor, data_loader, loss_fn, optimizer, n
 
     """
     num_samples = len(data_loader.dataset)  
+    batch_size = data_loader.batch_sampler.batch_size if hasattr(data_loader.batch_sampler, 'batch_size') else data_loader.batch_size
     num_batches = len(data_loader) 
 
-    scheduler = CosineAnnealingWarmRestarts(
-        optimizer, 
-        T_0=2, 
-        T_mult=2, 
-        eta_min=1e-6
-    )
-
-    wandb.init(
-        project="fine-tune-hnc-clip", 
-        name="clip-vision-fine-tuning",  
-        config={
-            "num_epochs": num_epochs,
-            "learning_rate": learning_rate,
-            "num_batches": num_batches,
-            "num_samples": num_samples
-        },
-    )
+    if dist.get_rank() == 0: 
+        wandb.init(
+            project="fine-tune-hnc-clip",
+            name="clip-vision-fine-tuning",
+            reinit=True,
+            config={
+                "num_epochs": num_epochs,
+                "learning_rate": learning_rate,
+                "batch_size": batch_size,
+                "num_batches": num_batches,
+                "num_samples": num_samples,
+            },
+        )
 
     model_engine.train()
 
@@ -69,39 +101,48 @@ def train_clip_model(model_engine, processor, data_loader, loss_fn, optimizer, n
     for param in model_engine.visual_projection.parameters():
         param.requires_grad = False
 
+    best_loss = float('inf')
+
     for epoch in range(num_epochs):
-        logging.info(f"Epoch {epoch + 1}/{num_epochs} begins.")
+        if dist.get_rank() == 0:
+            logging.info(f"Epoch {epoch + 1}/{num_epochs} begins.")
+       
         total_loss = 0.0
     
         for step, batch in enumerate(data_loader):
+            # logging.info(f"Rank {dist.get_rank()} is processing batch {step} with size {len(batch[0])}")
 
             # Preprocess batch data
             pixel_values, pos_text_inputs, neg_text_inputs = preprocess_text_and_images(batch, processor, device)
-
+            # logging.info("Finished preprocessing batch.")
+            
             # Forward pass for image and text embeddings
             image_embeddings = model_engine.get_image_features(pixel_values)
+            # logging.info("Finished forward pass.")
             with torch.no_grad():
                 pos_text_embeddings = model_engine.get_text_features(pos_text_inputs)  
                 neg_text_embeddings = model_engine.get_text_features(neg_text_inputs)
 
             loss = loss_fn(image_embeddings, pos_text_embeddings, neg_text_embeddings, model_engine)
+            # logging.info("Computed loss.")
 
             # Backpropagation
             optimizer.zero_grad()
             model_engine.backward(loss)
             model_engine.step()
-            scheduler.step(epoch + step / len(data_loader)) 
 
             total_loss += loss.item()
-            wandb.log({"batch_loss": loss.item(), "learning_rate": scheduler.get_last_lr()[0]})
+            if dist.get_rank() == 0:
+                wandb.log({"batch_loss": loss.item(), "current_lr": optimizer.param_groups[0]["lr"]})
 
+            if dist.get_rank() == 0 and step % 100 == 0:
+                logging.info(f"Epoch {epoch + 1}, Step {step}/{num_batches}, Loss: {loss.item():.4f}")
 
-            if step % 100 == 0:
-                logging.info(f"Epoch {epoch + 1}, Step {step}/{num_samples}, Loss: {loss.item()}")
 
         avg_loss = total_loss / num_batches
-        logging.info(f"Epoch {epoch + 1} completed. Average Loss: {avg_loss}")
-        wandb.log({"epoch_loss": avg_loss})
+        if dist.get_rank() == 0:
+            logging.info(f"Epoch {epoch + 1} completed. Average Loss: {avg_loss}")
+            wandb.log({"epoch_loss": avg_loss})
         
         # Save cache after each epoch
         epoch_cache_dir = f"./epoch_cache/epoch_{epoch + 1}"
@@ -109,6 +150,15 @@ def train_clip_model(model_engine, processor, data_loader, loss_fn, optimizer, n
         model_engine.save_pretrained(epoch_cache_dir)
         processor.save_pretrained(epoch_cache_dir)
         logging.info(f"Model and processor saved to {epoch_cache_dir} after epoch {epoch + 1}.")
+
+        # Save the best model
+        if avg_loss < best_loss:
+            best_loss = avg_loss
+            best_model_dir = f"./best_model/"
+            os.makedirs(best_model_dir, exist_ok=True)
+            model_engine.save_pretrained(best_model_dir)
+            processor.save_pretrained(best_model_dir)
+            logging.info(f"Best model saved to {best_model_dir}.")
 
     wandb.finish()
 
