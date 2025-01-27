cceb5059906200f35cb29500df124347b9fbd790
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-27 20:53:26 +0100

    Update run_evaluation.sh

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,29 +4,28 @@
 set -e
 
 # Variables
-# DATASET_PATH="./HNC/hnc_val_sampled_10_percent.json"  
-IMAGE_FOLDER="./gqa_dataset/images/images" 
-MODEL_NAME="openai/clip-vit-base-patch32"        
-DATASET_TYPE="hnc"   # 'hnc' or 'coco'
-BATCH_SIZE=32  
-
-# PYTHON_FILE="eval_distinguish.py"    
-PYTHON_FILE="eval_retrieval.py" 
+PYTHON_FILE="eval_distinguish.py"    
+# PYTHON_FILE="eval_retrieval.py" 
 
 # ===path for hnc===
+# DATASET_TYPE="hnc"   # 'hnc' or 'coco'
 # IMAGE_FOLDER="./gqa_dataset/images/images" 
-DATASET_PATH="./HNC/hnc_val_sampled_1_percent.json"
+# DATASET_PATH="./HNC/hnc_val_sampled_1_percent.json"
 # DATASET_PATH="./HNC/hnc_clean_strict_val.json"
 # DATASET_PATH="./HNC/hnc_val_sampled_10_percent.json"
 
 # ===path for coco===
-# IMAGE_FOLDER="./Coco/val2014" 
-# DATASET_PATH="./Coco/test_coco_aug_withneg.json"
+DATASET_TYPE="coco"  # 'hnc' or 'coco'
+IMAGE_FOLDER="./Coco/val2014" 
+DATASET_PATH="./Coco/test_coco_aug_withneg.json"
+  
 
 # ===model_name===
 # MODEL_NAME="openai/clip-vit-base-patch32" 
 # MODEL_NAME='Nano1337/openclip-negclip' 
-# MODEL_NAME='WenWW/HNC_CLIP_B32_D5' 
+MODEL_NAME='WenWW/HNC_CLIP_B32_1.5' 
+
+BATCH_SIZE=32  
 
 python $PYTHON_FILE --dataset $DATASET_PATH \
                     --image_folder $IMAGE_FOLDER \
