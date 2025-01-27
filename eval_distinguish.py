63dc6f34bf31cfe231ff6a54abf99e7fb9711a14
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-27 18:38:09 +0100

    Add files via upload

---
+++ eval_distinguish.py
@@ -0,0 +1,88 @@
+import os
+import json
+import random
+import torch
+from tqdm import tqdm
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+from torch.utils.data import Dataset, DataLoader
+import argparse
+
+from load_data import LoadHNCPair, LoadCOCOPair
+
+
+def distinguish_clip(model, processor, dataset, device, batch_size=32):
+    """
+     The accuracy of the model in distinguishing positive from negative captions.
+    """
+    model.eval()
+    total_samples = 0
+    correct_predictions = 0
+
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, collate_fn=lambda x: x)
+
+    with torch.no_grad():
+        for batch in tqdm(data_loader, desc="Evaluating", leave=False):
+            image_paths, pos_captions, neg_captions = zip(*batch)
+
+            images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+            inputs = processor(images=images, return_tensors="pt", padding=True).to(device)
+
+            # Randomly shuffle between positive and negative captions
+            captions = []
+            labels = [] 
+            for pos, neg in zip(pos_captions, neg_captions):
+                if random.random() < 0.5:
+                    captions.append(pos)
+                    labels.append(1)
+                else:
+                    captions.append(neg)
+                    labels.append(0)
+
+
+            text_inputs = processor(text=captions, return_tensors="pt", padding=True).to(device)
+
+            image_features = model.get_image_features(**inputs)
+            text_features = model.get_text_features(**text_inputs)
+
+            image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+            text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+            similarities = torch.matmul(image_features, text_features.T).diagonal()
+
+            # Convert similarities to predictions
+            predictions = (similarities > 0.5).long().tolist()
+
+            # Compute accuracy
+            correct_predictions += sum(p == l for p, l in zip(predictions, labels))
+            total_samples += len(labels)
+
+    accuracy = correct_predictions / total_samples
+    return accuracy
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Evaluate HNC CLIP Model")
+    parser.add_argument("--dataset", type=str, required=True, help="Path to the dataset JSON file.")
+    parser.add_argument("--image_folder", type=str, required=True, help="Path to the image folder.")
+    parser.add_argument("--model_name", type=str, required=True, help="Name of the CLIP model to use.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for evaluation.")
+    parser.add_argument("--dataset_type", type=str, required=True, choices=["hnc", "coco"], help="Type of dataset (hnc or coco).")
+
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = CLIPModel.from_pretrained(args.model_name).to(device)
+    processor = CLIPProcessor.from_pretrained(args.model_name)
+
+    eval_annotations = json.load(open(args.dataset))
+
+    if args.dataset_type == "hnc":
+        eval_dataset = LoadHNCPair(annotations=eval_annotations, image_folder=args.image_folder)
+    elif args.dataset_type == "coco":
+        eval_dataset = LoadCOCOPair(annotations=eval_annotations, image_folder=args.image_folder)
+
+    accuracy = distinguish_clip(model, processor, eval_dataset, device, batch_size=args.batch_size)
+    print(f"Model {args.model_name}: Validation Accuracy: {accuracy * 100:.2f}%")
+
+if __name__ == "__main__":
+    main()
---
+++ run_evaluation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Ensure the script stops on errors
+set -e
+
+# Variables
+DATASET_PATH="/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_val_sampled_10_percent.json"  
+IMAGE_FOLDER="/mount/studenten/team-lab-cl/data2024/w/data/thes/gqa_dataset/images/images" 
+MODEL_NAME="WenWW/HNC_CLIP_B32_1.0"        
+DATASET_TYPE="hnc"   # 'hnc' or 'coco'
+BATCH_SIZE=32                            
+PYTHON_FILE="eval_distinguish.py"       
+
+
+# ===path for hnc===
+# IMAGE_FOLDER = "/mount/studenten/team-lab-cl/data2024/w/data/thes/gqa_dataset/images/images" 
+# DATASET_PATH = "/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_val_sampled_1_percent.json"
+# DATASET_PATH = "/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_clean_strict_val.json"
+# DATASET_PATH = "/mount/studenten/team-lab-cl/data2024/w/data/thes/HNC/hnc_val_sampled_10_percent.json"
+
+# ===path for coco===
+# IMAGE_FOLDER = "..." 
+# DATASET_PATH = "..."
+
+# ===model_name===
+# MODEL_NAME= "openai/clip-vit-base-patch32" 
+# MODEL_NAME= 'Nano1337/openclip-negclip' 
+# MODEL_NAME= 'WenWW/HNC_CLIP_B32_D5' 
+
+
+python $PYTHON_FILE --dataset $DATASET_PATH \
+                    --image_folder $IMAGE_FOLDER \
+                    --model_name $MODEL_NAME \
+                    --dataset_type $DATASET_TYPE \
+                    --batch_size $BATCH_SIZE
