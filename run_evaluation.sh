915619bfbbfa4c0bda1f37dd3a485d913064e510
Wen <150439278+WenW186079@users.noreply.github.com>
2025-02-01 17:22:13 +0100

    Update run_evaluation.sh

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -9,23 +9,41 @@ PYTHON_FILE="eval_distinguish.py"
 
 
 # ===path for hnc===
-DATASET_TYPE="hnc"   # 'hnc' or 'coco'
-IMAGE_FOLDER="./gqa_dataset/images/images" 
-DATASET_PATH="./HNC/hnc_val_sampled_1_percent.json"
+# DATASET_TYPE="hnc"   # 'hnc' or 'coco'
+# IMAGE_FOLDER="./gqa_dataset/images/images" 
+# DATASET_PATH="./HNC/hnc_val_sampled_1_percent.json"
 # DATASET_PATH="./HNC/hnc_clean_strict_val.json"
 # DATASET_PATH="./HNC/hnc_val_sampled_10_percent.json"
 
 
 # ===path for coco===
-# DATASET_TYPE="coco"  # 'hnc' or 'coco'
-# IMAGE_FOLDER="./Coco/val2014" 
-# DATASET_PATH="./Coco/test_coco_aug_withneg.json"
+DATASET_TYPE="coco"  # 'hnc' or 'coco'
+IMAGE_FOLDER="./Coco/val2014" 
+DATASET_PATH="./Coco/test_coco_aug_withneg.json"
   
 
 # ===model_name===
-# MODEL_NAME="openai/clip-vit-base-patch32" 
-# MODEL_NAME='Nano1337/openclip-negclip' 
-MODEL_NAME='WenWW/HNC_CLIP_B32_1.5' 
+MODEL_NAME="openai/clip-vit-base-patch32,\
+Nano1337/openclip-negclip,\
+WenWW/HNC_D1-15_epoch1,\
+WenWW/HNC_D1-15_epoch2,\
+WenWW/HNC_D1-15_epoch3,\
+WenWW/HNC_D1-15_epoch4,\
+WenWW/HNC_D1-15_epoch5,\
+WenWW/HNC_CLIP_B32_1.0,\
+WenWW/HNC_CLIP_B32_1.5,\
+WenWW/HNC_clip_D2.0,\
+WenWW/HNC_CLIP_B32_D5,\
+WenWW/HNC_D1-3_epoch1,\
+WenWW/HNC_D1-3_epoch2,\
+WenWW/HNC_D1-3_epoch3,\
+WenWW/HNC_D1-3_epoch4,\
+WenWW/HNC_D1-3_epoch5,\
+WenWW/HNC_D1-1.5_2048_epoch1,\
+WenWW/HNC_D1-1.5_2048_epoch2,\
+WenWW/HNC_D1-1.5_2048_epoch3,\
+WenWW/HNC_D1-1.5_2048_epoch4"
+
 
 BATCH_SIZE=32  
 
