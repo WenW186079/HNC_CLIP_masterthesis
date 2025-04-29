966752489ab223af4ef409a645aced51bdd2f1e7
Wen <150439278+WenW186079@users.noreply.github.com>
2025-04-29 16:02:15 +0200

    Update run_test_data.sh

--- eval/run_test_data.sh
+++ eval/run_test_data.sh
@@ -5,33 +5,23 @@ PROJECT_ROOT=$(pwd)
 
 MODEL_TYPE="finetuned"  # "base","finetuned"
 
-CHECKPOINTS="$PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_1_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_2_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_3_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_4_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_5_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_6_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_7_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_8_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_9_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_10_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_11_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_12_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_13_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_14_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_15_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_16_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_17_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_18_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_19_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_20_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_21_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_22_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_23_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_24_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_25_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_26_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_27_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_28_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_29_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_30_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_31_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_32_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_33_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_34_full_encoder.pt $PROJECT_ROOT/checkpoints_HNC1_S_full/epoch_35_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_standard_S_full/epoch_1_full_encoder.pt $PROJECT_ROOT/checkpoints_standard_S_full/epoch_2_full_encoder.pt $PROJECT_ROOT/checkpoints_standard_S_full/epoch_3_full_encoder.pt $PROJECT_ROOT/checkpoints_standard_S_full/epoch_4_full_encoder.pt $PROJECT_ROOT/checkpoints_standard_S_full/epoch_5_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hncd1_50_S_full_256/epoch_1_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full_256/epoch_2_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full_256/epoch_3_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full_256/epoch_4_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hncd1_50_S_full/epoch_1_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full/epoch_2_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full/epoch_3_full_encoder.pt $PROJECT_ROOT/checkpoints_hncd1_50_S_full/epoch_4_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hnc1_S_full_last/epoch_5_full_encoder.pt $PROJECT_ROOT/checkpoints_hnc1_S_full_last/epoch_10_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hnc10_S_full/epoch_5_full_encoder.pt $PROJECT_ROOT/checkpoints_hnc10_S_full/epoch_10_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hnc1_S_text/epoch_10_text_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hnc1_S_full/epoch_10_full_encoder.pt $PROJECT_ROOT/checkpoints_hnc1_S_full/epoch_20_full_encoder.pt $PROJECT_ROOT/checkpoints_hnc1_S_full/epoch_30_full_encoder.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_dpo_S/final_model.pt"
-# CHECKPOINTS="$PROJECT_ROOT/checkpoints_hnc_d0-100_L/epoch_3.pt"
+paths=( "$PROJECT_ROOT/models/HNC_L2_1_S_text/epoch_"{1..100}"_full_encoder.pt" )
+CHECKPOINTS="${paths[*]}"
 
-
-
-FINETUNE_TYPE='vision_encoder'    # 'text_encoder','vision_encoder','full_encoder'
-EVAL_METHOD="random"   # "cosine", "plot", "random","distinguish"
+FINETUNE_TYPE='full_encoder'     # 'text_encoder','vision_encoder','full_encoder'
+EVAL_METHOD="random"             # "cosine", "plot", "random", "distinguish"
 MODEL_NAME="ViT-B/32"
 BATCH_SIZE=32
 
-# LOADER_TYPE='hnc'
-# TEST_JSON="$PROJECT_ROOT/data/HNC/hnc_clean_strict_test.json"
-# IMAGES_PATH="$PROJECT_ROOT/data/gqa_dataset/images/images"
+LOADER_TYPE='hnc'
+TEST_JSON="$PROJECT_ROOT/data/HNC/hnc_clean_strict_test.json"
+IMAGES_PATH="$PROJECT_ROOT/data/gqa_dataset/images/images"
 
-LOADER_TYPE='coco'
-TEST_JSON="$PROJECT_ROOT/data/Coco/test_coco_aug_withneg.json"
-IMAGES_PATH="$PROJECT_ROOT/data/Coco/val2014"
+# LOADER_TYPE='coco'
+# TEST_JSON="$PROJECT_ROOT/data/Coco/test_coco_aug_withneg.json"
+# IMAGES_PATH="$PROJECT_ROOT/data/Coco/val2014"
 
-CUDA_VISIBLE_DEVICES=6  python eval/evaluation.py \
+CUDA_VISIBLE_DEVICES=3  python eval/evaluation.py \
     --model_type $MODEL_TYPE \
     --checkpoint_path $CHECKPOINTS \
     --eval_method $EVAL_METHOD \
