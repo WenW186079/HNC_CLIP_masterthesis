ea85a268fd8ef5d14269580dedc903768c3c996f
Wen <150439278+WenW186079@users.noreply.github.com>
2025-06-02 01:11:46 +0200

    Update run_test_data.sh

--- eval/run_test_data.sh
+++ eval/run_test_data.sh
@@ -3,32 +3,37 @@
 cd "$(dirname "$0")/.."
 PROJECT_ROOT=$(pwd)
 
+
 paths=(
-  "$PROJECT_ROOT/models/HNC_KL_10_S_last/epoch_"{1..2}"_last_encoder.pt"
+
+  # "$PROJECT_ROOT/models/HNC_KL_1_100k_full_20/epoch_12_full_encoder.pt" 
+
 )
 CHECKPOINTS="${paths[*]}"
+OUTPUT_CSV="./coco_05.csv"
 
 MODEL_TYPE="finetuned"              # "base","finetuned"
-FINETUNE_TYPE='last_encoder'        # 'text_encoder','vision_encoder','full_encoder',"last_encoder"
-EVAL_METHOD="random+distinguish"    # "cosine", "plot", "random", "distinguish", 'random+distinguish'
+FINETUNE_TYPE='vision_encoder'        # 'text_encoder','vision_encoder','full_encoder',"last_encoder"
+EVAL_METHOD="random+thresholds"    # "cosine", "plot", "random", "thresholds", 'random+thresholds'
 MODEL_NAME="ViT-B/32"
 BATCH_SIZE=32
 
 LOADER_TYPE='hnc'
-TEST_JSON="$PROJECT_ROOT/data/HNC/hnc_clean_strict_test.json"
+TEST_PATH="$PROJECT_ROOT/data/HNC/filtered_test_data.json"
 IMAGES_PATH="$PROJECT_ROOT/data/gqa_dataset/images/images"
 
 # LOADER_TYPE='coco'
-# TEST_JSON="$PROJECT_ROOT/data/Coco/test_coco_aug_withneg.json"
+# TEST_PATH="$PROJECT_ROOT/data/Coco/test_coco_aug_withneg.json"
 # IMAGES_PATH="$PROJECT_ROOT/data/Coco/val2014"
 
-CUDA_VISIBLE_DEVICES=3 python eval/evaluation.py \
+CUDA_VISIBLE_DEVICES=0 python eval/evaluation.py \
     --model_type $MODEL_TYPE \
     --checkpoint_path $CHECKPOINTS \
     --eval_method $EVAL_METHOD \
     --model_name $MODEL_NAME \
-    --test_json $TEST_JSON \
+    --test_json $TEST_PATH \
     --images_path $IMAGES_PATH \
     --batch_size $BATCH_SIZE \
     --finetune_mode $FINETUNE_TYPE \
-    --loader_type $LOADER_TYPE
+    --loader_type $LOADER_TYPE \
+    --output_csv $OUTPUT_CSV
